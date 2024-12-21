c14b05a8d157351ec04f8c513bc3fcf666525f4b
Anatoliy Kuznetsov <tolya.kuznetsov117@gmail.com>
2024-12-21 15:13:32 -0500

    16

--- 16/16.py
+++ 16/16.py
@@ -40,10 +40,14 @@ class Maze:
     step_cost = 1
     MAX_DISTANCE = 18446744073709551615
     def __init__(self, maze_text: list[str]):
+        self.width = len(maze_text[0]) - 1
+        self.height = len(maze_text)
         self.nodes = set()
         self.adjacencies = defaultdict(lambda: {})
         self.distances_from_start = {}
         self.shortest_distance_found = self.MAX_DISTANCE
+        self.preceding_nodes = defaultdict(lambda: set())
+        self.cells_along_path = set()
         for i, line in enumerate(maze_text):
             for j, char in enumerate(line):
                 if char in ("#", "\n"):
@@ -52,6 +56,7 @@ class Maze:
                     self.start = ((i, j), "E")
                     self.nodes.add(self.start)
                     self.distances_from_start[self.start] = 0
+                    self.cells_along_path.add((i, j))
 
                     turned_starts = [
                         ((i, j), "N"),
@@ -65,6 +70,7 @@ class Maze:
                     self.update_adjacencies(self.start)
                     continue
                 if char == "E":
+                    self.cells_along_path.add((i, j))
                     self.ends = [
                         ((i, j), "N"),
                         ((i, j), "E"),
@@ -131,9 +137,14 @@ class Maze:
     def update_distances_from_start(self, node: tuple[tuple[int, int], str]):
         for neighbor in self.adjacencies[node]:
             distance = self.distances_from_start[node] + self.adjacencies[node][neighbor]
-            if distance >= self.distances_from_start[neighbor] or distance >= self.shortest_distance_found:
+            if distance > self.distances_from_start[neighbor] or distance > self.shortest_distance_found:
+                continue
+            elif distance == self.distances_from_start[neighbor]:
+                self.preceding_nodes[neighbor].add(node)
                 continue
             self.distances_from_start[neighbor] = distance
+            self.preceding_nodes[neighbor].clear()
+            self.preceding_nodes[neighbor].add(node)
             if neighbor in self.ends:
                 self.shortest_distance_found = min(
                     self.shortest_distance_found,
@@ -145,9 +156,46 @@ class Maze:
     def solve(self) -> int:
         self.update_distances_from_start(self.start)
         return self.shortest_distance_found
+    
+    def add_cells_along_path(self, node: tuple[tuple[int, int], str]):
+        if self.start in self.preceding_nodes[node]:
+            return
+        self.cells_along_path.add(node[0])
+        for preceding_node in self.preceding_nodes[node]:
+            self.add_cells_along_path(preceding_node)
+
+    def count_cells_along_path(self) -> int:
+        destination_states = set()
+        for state in self.ends:
+            if self.distances_from_start[state] > self.shortest_distance_found:
+                continue
+            destination_states.add(state)
+        for state in destination_states:
+            self.add_cells_along_path(state)
+        return len(self.cells_along_path)
+    
+    def __repr__(self):
+        drawing = [["" for _ in range(self.width + 1)] for _ in range(self.height)]
+        result = ""
+        for i in range(self.height):
+            for j in range(self.width):
+                if ((i, j), "E") == self.start:
+                    drawing[i][j] = "S"
+                elif ((i, j), "E") in self.ends:
+                    drawing[i][j] = "E"
+                elif (i, j) in self.cells_along_path:
+                    drawing[i][j] = "O"
+                elif ((i, j), "N") in self.nodes:
+                    drawing[i][j] = "."
+                else:
+                    drawing[i][j] = "#"
+            result += "".join(drawing[i]) + "\n"
+        return result
 
 with open("input.txt", "r") as f:
     lines = f.readlines()
 maze = Maze(lines)
 shortest_path_length = maze.solve()
 print(f"Shortest path length is {shortest_path_length}")
+cell_along_path_count = maze.count_cells_along_path()
+print(f"There are {cell_along_path_count} cells on a shortest path")
